9dceff04bbda64cdf101645a081b85162f706dc0
Asnaui Pangcatan <tdh.asnaui@gmail.com>
2023-10-23 15:17:24 +0800

    15px

--- resources/views/pdf/maipp.blade.php
+++ resources/views/pdf/maipp.blade.php
@@ -326,7 +326,7 @@
             </tr>
         </table>
     </div>
-    <div class="mt-3" style="float:right">
+    <div style="float:right;margin-top:15px">
         <div>AHQM-REC-FM-03 Rev.0</div>
         <div>20 February 2023</div>
     </div>
